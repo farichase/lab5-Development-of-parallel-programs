5a1a3333920c338a9e8aa7c12fb6a6f5e0f3cf16
farichase <fbazartinova@mail.ru>
2020-12-07 22:31:11 +0300

    Scripted auto-commit on change (2020-12-07 22:31:10) by gitwatch.sh

--- lab5/src/main/java/lab5/Async.java
+++ lab5/src/main/java/lab5/Async.java
@@ -2,11 +2,14 @@ package lab5;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
 
 public class Async {
     private ActorRef cacheActor;
     public Async(ActorSystem system){
         this.cacheActor = system.actorOf(CacheActor.props(), "cache");
     }
-    public static Flow<>
+    public static Flow<> createRouteFlow(ActorMaterializer materializer){
+        return Flow.
+    }
 }

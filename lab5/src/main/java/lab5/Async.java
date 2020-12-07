c94da1397fd347cb1722a6d2ac5ddc14f5cf9898
farichase <fbazartinova@mail.ru>
2020-12-07 23:09:03 +0300

    Scripted auto-commit on change (2020-12-07 23:09:02) by gitwatch.sh

--- lab5/src/main/java/lab5/Async.java
+++ lab5/src/main/java/lab5/Async.java
@@ -4,13 +4,15 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.Query;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import javafx.util.Pair;
 
 public class Async {
     private ActorRef cacheActor;
-    private int PARALLELIZM_PARAMETR = 1;
+    private static int PARALLELIZM = 1;
+    private static int TIMEOUT = 
     public Async(ActorSystem system){
         this.cacheActor = system.actorOf(CacheActor.props(), "cache");
     }
@@ -20,11 +22,10 @@ public class Async {
             String url = query.get("testUrl").get();
             int count = Integer.parseInt(query.get("count").get());
             return new Pair<>(url, count);
-        }).mapAsync(
+        }).mapAsync(PARALLELIZM, (Pair<String, Integer> pair) -> {
+            return Patterns.ask(this.cacheActor, pair, )
 
 
-
-
-        );
+        });
     }
 }

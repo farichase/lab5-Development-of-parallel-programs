debde060993d6c594414f37ba93b6016c79fce9c
farichase <fbazartinova@mail.ru>
2020-12-07 19:54:37 +0300

    Scripted auto-commit on change (2020-12-07 19:54:34) by gitwatch.sh

--- lab5/src/main/java/lab5/AkkaApp.java
+++ lab5/src/main/java/lab5/AkkaApp.java
@@ -2,13 +2,16 @@ package lab5;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
 import java.io.IOException;
+import java.util.concurrent.CompletionStage;
 
 public class AkkaApp {
     public static void main(String[] args) throws IOException {
@@ -16,6 +19,14 @@ public class AkkaApp {
         ActorSystem system = ActorSystem.create("routes");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeF
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createRouteFlow();
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080");
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 }

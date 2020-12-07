d844afa44a6d6c9bd3dd84833ce4b05bbf75609c
farichase <fbazartinova@mail.ru>
2020-12-07 22:26:05 +0300

    Scripted auto-commit on change (2020-12-07 22:26:04) by gitwatch.sh

--- lab5/src/main/java/lab5/CacheActor.java
+++ lab5/src/main/java/lab5/CacheActor.java
@@ -1,4 +1,11 @@
 package lab5;
 
-public class CacheActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class CacheActor extends AbstractActor{
+    @Override
+    public Receive createReceive(){
+        return ReceiveBuilder.create().build();
+    }
 }

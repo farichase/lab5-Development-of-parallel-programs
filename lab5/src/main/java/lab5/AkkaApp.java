ab6548f6719bb4d50da0b409ea5fa66048486013
farichase <fbazartinova@mail.ru>
2020-12-07 19:50:53 +0300

    Scripted auto-commit on change (2020-12-07 19:50:53) by gitwatch.sh

--- lab5/src/main/java/lab5/AkkaApp.java
+++ lab5/src/main/java/lab5/AkkaApp.java
@@ -1,2 +1,21 @@
-package lab5;public class AkkaApp {
+package lab5;
+
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.io.IOException;
+
+public class AkkaApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("start");
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeF
+    }
 }

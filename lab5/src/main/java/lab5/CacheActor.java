be97ee2da569fedf299631dc9a60d2fb8347f2a3
farichase <fbazartinova@mail.ru>
2020-12-08 12:57:41 +0300

    Scripted auto-commit on change (2020-12-08 12:57:41) by gitwatch.sh

--- lab5/src/main/java/lab5/CacheActor.java
+++ lab5/src/main/java/lab5/CacheActor.java
@@ -4,7 +4,11 @@ import akka.actor.AbstractActor;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheActor extends AbstractActor{
+    private Map<String, Float> cache = new HashMap<>();
     @Override
     public Receive createReceive(){
         return ReceiveBuilder.create().build();
